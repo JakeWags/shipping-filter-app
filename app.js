3338aec96eba97f997fd1be91bd545bca7240e08
Jake Wags <jakewagoneredu@gmail.com>
2018-07-31 20:53:56 -0700

    fixed string split error

--- app.js
+++ app.js
@@ -28,12 +28,11 @@ app.post('/', (req, res) => {
   console.log(`Order id: ${req.body.id}`);
   console.log(`Shipping Method: ${req.body.shipping_lines[0].title}`);
   stringTitle = req.body.shipping_lines[0].title;
-  title = stringTitle.split(' ( ');
-  title = title.slice(0, -1);
+  title = stringTitle.split(' (')[0];
   id = req.body.id;
   let addTag = {
     "order": {
-      "tags": title[0] // tag to be added to the orders
+      "tags": title // tag to be added to the orders
     }
   }
   // Puts "In-Store" tag on orders with "In-Store" shipping method
